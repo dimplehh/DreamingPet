608be7ce5444efff5bf0671216971e40941006ce
dimplehh <dimplehh@naver.com>
2023-03-19 12:52:43 +0900

    배경 적용

---
+++ Assets/Scripts/Utils/Background2.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background2 : MonoBehaviour
+{
+    [SerializeField]
+    float speed;
+    [SerializeField]
+    int startIndex;
+    [SerializeField]
+    int endIndex;
+    [SerializeField]
+    Transform[] sprites;
+    float viewHeight;
+
+    private void Awake()
+    {
+        viewHeight = 55.5f;
+    }
+    void Update()
+    {
+        Vector3 curPos = transform.position;
+        Vector3 nextPos = Vector3.up * speed * Time.deltaTime;
+        transform.position = curPos + nextPos;
+
+        if (sprites[endIndex].position.y > 0)
+        {
+            Vector3 downSprite = sprites[endIndex].localPosition;
+            sprites[startIndex].transform.localPosition = downSprite + Vector3.down * viewHeight;
+
+            //#.Cursor Index Change
+            int endIndexSave = endIndex;
+            endIndex = startIndex;
+            startIndex = (endIndexSave - 1 == -1) ? sprites.Length - 1 : endIndexSave - 1;
+
+        }
+    }
+}
