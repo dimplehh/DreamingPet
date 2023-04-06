7241f9b44e8cde1df735259f941dfeeaa7c26ac9
dimplehh <dimplehh@naver.com>
2023-04-06 20:02:00 +0900

    효과음 온오프 우선 홈화면만

---
+++ Assets/Scripts/Managers/SoundManager2.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager2 : MonoBehaviour
+{
+    private static SoundManager2 _instance;
+
+    public AudioSource effectSound;
+    public bool soundOn;
+    private void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this;
+            soundOn = true;
+        }
+        else if (_instance != this)
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void EffectSoundPlay(AudioClip clip)
+    {
+        if(soundOn)
+        {
+            effectSound.clip = clip;
+            effectSound.volume = 0.1f;
+            effectSound.Play();
+        }
+    }
+}
