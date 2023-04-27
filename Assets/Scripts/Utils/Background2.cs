b4911a6c64a236fdb540bcd856cb5d17b16720fc
gusgml <dimplehh@naver.com>
2023-04-27 12:27:18 +0900

    no message

--- Assets/Scripts/Utils/Background2.cs
+++ Assets/Scripts/Utils/Background2.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
-using UnityEditor.Experimental.GraphView;
 using System;
 
 public class Background2 : MonoBehaviour
